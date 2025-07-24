1dd32429228ffbd761a56dea1ebb5bab1cf7410b
thanky1810 <thanky4444@gmail.com>
2025-07-24 22:50:21 +0700

    useCallback

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./useRef";
+import App from "./useCallback";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App></App>
---
+++ src/useCallback.jsx
@@ -0,0 +1,40 @@
+import { useState, memo, useCallback } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  const addTodo = useCallback(() => {
+    setTodos((t) => [...t, "New Todo"]);
+  }, [todos]);
+
+  return (
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+const Todos = memo(({ todos, addTodo }) => {
+  console.log("child render");
+  return (
+    <>
+      <h2>My Todos</h2>
+      {todos.map((todo, index) => {
+        return <p key={index}>{todo}</p>;
+      })}
+      <button onClick={addTodo}>Add Todo</button>
+    </>
+  );
+});
+
+export default App;
